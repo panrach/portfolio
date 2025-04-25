89cd1429cf6c4a2f69f95383270d67b9bc684dde
Rachel Pan <r.pan@mail.utoronto.ca>
2025-04-25 12:49:24 -0400

    refactor experience

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { initializeAboutMe } from './pages/about.js';
 import { initializeBlossomAnimation } from './pages/blossom.js';
 import { initializeLightbox } from './pages/lightbox.js';
+import { initializeExperience } from './pages/experience.js';
 
 // Function to show the selected section and hide others
 function toggleSections(event) {
@@ -185,82 +186,7 @@ document.addEventListener("DOMContentLoaded", () => {
         projectList.appendChild(projectCard);
     });
     initializeLightbox();
-
-    // Experience array
-    const experiences = [
-        {
-            title: "Developer",
-            company: "Bank of Canada",
-            duration: "May 2025 - August 2025",
-            description: ["✿ Incoming this summer"],
-            image: "./static/BankOfCanada.svg.webp"
-        },
-        {
-            title: "DevOps Intern",
-            company: "SS&C Technologies",
-            duration: "September 2023 - April 2024",
-            description: ["✿ Proactively self-learned and introduced Node-RED to a team of software developers through a live demo, providing a streamlined tool for process automation.", 
-                          "✿ Modernized a legacy environment tracking application with Node-RED and JavaScript, improving codebase modularity and reducing technical debt, resulting in a 30% reduction in maintenance time.", 
-                          "✿ Extended the application with remote environments and GitHub integration using ShellScript, ensuring 99% configuration consistency across development, staging, and production environments.", 
-                          "✿ Optimized the application's logs by integrating Log4j into real-time console outputs and regularly updated files, reducing log retrieval time by 30% and enabling faster issue diagnosis.",
-                          "✿ Deployed the application with Docker, Kubernetes, and Helm on RedHat OpenShift to ensure continuous operation and seamless scalability.",
-                          "✿ Automated daily report analysis with Perl and Node-RED, guaranteeing reliability and eliminating 4 hours of administrative tasks per month."],
-            image: "./static/SS&C_Technologies.png"
-        },
-    ];
-
-    // Get the experience list container
-    const experienceList = document.querySelector(".experience-list");
-
-    experiences.forEach((experience, index) => {
-        const timelineItem = document.createElement('div');
-        timelineItem.className = 'timeline-item';
-        if (index % 2 === 0) {
-            timelineItem.classList.add('left');
-        } else {
-            timelineItem.classList.add('right');
-        }
-
-        const timelineDot = document.createElement('div');
-        timelineDot.className = 'timeline-dot';
-
-        const timelineContent = document.createElement('div');
-        timelineContent.className = 'timeline-content';
-
-        const title = document.createElement('h3');
-        title.className = 'experience-title'; // Add class for styling
-        title.textContent = experience.title;
-
-        const company = document.createElement('p');
-        company.className = 'company-name'; // Add class for styling
-        company.textContent = experience.company;
-
-        const duration = document.createElement('div');
-        duration.className = 'timeline-duration';
-        duration.textContent = experience.duration;
-
-        const experienceImage = document.createElement('img');
-        experienceImage.src = experience.image; // Replace with the actual image path
-        experienceImage.alt = 'Experience Image';
-        experienceImage.className = 'experience-image';
-
-        const descriptionList = document.createElement('ul');
-        experience.description.forEach(point => {
-            const listItem = document.createElement('li');
-            listItem.textContent = point;
-            descriptionList.appendChild(listItem);
-        });
-        timelineContent.appendChild(experienceImage); // Add the image to the timeline content
-        timelineContent.appendChild(title);
-        timelineContent.appendChild(company);
-        timelineContent.appendChild(descriptionList);
-
-        timelineItem.appendChild(timelineDot);
-        timelineItem.appendChild(timelineContent);
-        timelineItem.appendChild(duration); // Append duration to timelineItem
-
-        experienceList.appendChild(timelineItem);
-    });
+    initializeExperience();
 
     // Skills array
     const skills = [
